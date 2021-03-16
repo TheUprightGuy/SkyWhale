024f358e3eefe22031502c1530b5a29d7886236c
Jacob Gallagher <Jacob.Gallagher1@mds.ac.nz>
2021-03-16 16:29:02 +1300

    Trailing islands major fix/update - Flocking, spawnPos, rotation, etc

--- Assets/Scripts/Island/CollectableIsland/IslandSpawner.cs
+++ Assets/Scripts/Island/CollectableIsland/IslandSpawner.cs
@@ -1,32 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Island.CollectableIsland;
 using UnityEngine;
 
 public class IslandSpawner : MonoBehaviour
 {
+    public List<GameObject> trailingIslands;
     public Transform followPoint;
     public GameObject islandPrefab;
+    private float _spawnHeightDifference = 15f;
+    public GameObject whale;
     
     // Start is called before the first frame update
     void Start()
     {
         CallbackHandler.instance.spawnCollectableIsland += SpawnIsland;
+        _spawnHeightDifference = 15f;
+        trailingIslands.Add(whale);
+    }
+
+    private void Update()
+    {
+        foreach (var trailingIsland in trailingIslands.Where(trailingIsland => trailingIsland.GetComponent<IslandTrailing>() != null))
+        {
+            trailingIsland.GetComponent<IslandTrailing>().Flock(trailingIslands);
+        }
     }
 
     public void SpawnIsland()
     {
         //Calculate spawn position/rotation
-        Vector3 prevIslandPos = gameObject.GetComponent<ChainController>().GetPreviousIslandPosition(0).position;
-        Vector3 spawnPos = prevIslandPos - gameObject.GetComponent<ChainController>().GetPreviousIslandPosition(0).forward * 80f;
-        var islandRotationVector = Vector3.RotateTowards(Vector3.one, prevIslandPos, 7f, 100f);
-        var islandRotation = Quaternion.FromToRotation(transform.forward, islandRotationVector);
+        GameObject prevIsland = trailingIslands[trailingIslands.Count - 1];
+        Vector3 prevIslandPos = prevIsland.transform.position;
+        Vector3 spawnPos = prevIslandPos - whale.transform.forward * 40f;
+        if (_spawnHeightDifference == 0)//Island being spawned isn't the first island
+        {
+            //Spawn island along vector between it's transform and follow point
+            Vector3 prevIslandFollowPoint = prevIsland.GetComponent<IslandTrailing>().followPoint.position;
+            spawnPos = prevIslandPos + (prevIslandPos - prevIslandFollowPoint);
+        }
+       
+        /*var islandRotationVector = Vector3.RotateTowards(Vector3.one, prevIslandPos, 7f, 100f);
+        var islandRotation = Quaternion.FromToRotation(transform.forward, islandRotationVector);*/
+
+        var islandRotation = prevIsland.transform.rotation;
         
         //Spawn island
         var island = Instantiate(islandPrefab, spawnPos, islandRotation);
         
         //Set follow point for new island and update follow point for next island
-        island.GetComponent<IslandTrailing>().followPoint = followPoint;
+        var islandTrailing = island.GetComponent<IslandTrailing>();
+        islandTrailing.followPoint = followPoint;
         followPoint = island.transform;
+        
+        //If spawned island is the first then adjust follow height by spawnHeightDifference
+        islandTrailing.yDistanceBelowIsland = _spawnHeightDifference;
+        //All additional islands follow at the same height as their follow point
+        _spawnHeightDifference = 0f;
+        
+        trailingIslands.Add(island);
     }
 }
--- Assets/Scripts/Island/CollectableIsland/IslandTrailing.cs
+++ Assets/Scripts/Island/CollectableIsland/IslandTrailing.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Island.CollectableIsland
@@ -8,10 +10,12 @@ namespace Island.CollectableIsland
         [Header("Follow Point Variables")]
         public Transform followPoint;
         public float minDistance = 20f;
+        private Vector3 _flockingVector = Vector3.zero;
+        public float flockingStrength = 1.0f;
         
         [Header("Island Variables")]
         public Rigidbody islandRigidBody;
-        public float followStrength = 0.01f;
+        public float followStrength = 100f;
         public float yDistanceBelowIsland = 15.0f;
         
         //Constant variables
@@ -22,8 +26,6 @@ namespace Island.CollectableIsland
         public float bobTime = 3f;
 
         [Header("Bobbing Variables")] 
-        public float rotationTime = 0.5f;
-        private LTDescr _rotateTween;
         private LTDescr _bobTween;
 
         private void Start()
@@ -33,7 +35,8 @@ namespace Island.CollectableIsland
             transform.position = new Vector3(transform.position.x, followPoint.position.y - yDistanceBelowIsland,
                 transform.position.z);
             StartCoroutine(BobIsland(1f));
-            _rotateTween = RotateTowardsIsland();
+            //_rotateTween = RotateTowardsIsland();
+            RotateTowardsIsland();
         }
 
         /// <summary>
@@ -58,12 +61,7 @@ namespace Island.CollectableIsland
             float distanceToFollowPoint = followPointVector.magnitude;
             float slowingDistance = minDistance * SlowingDistanceMultiplier;
             TrailFollowPoint(distanceToFollowPoint, slowingDistance, followPointVector);
-            
-            //Rotate to face island
-            if (!LeanTween.isTweening(_rotateTween.id))
-            {
-                _rotateTween = RotateTowardsIsland();
-            }
+            RotateTowardsIsland();
         }
 
         /// <summary>
@@ -83,7 +81,9 @@ namespace Island.CollectableIsland
             else
             {
                 //Add trailing force to move in the direction of the follow point
-                islandRigidBody.AddForce(followPointVector * followStrength, ForceMode.Impulse);
+                Vector3 forceDirection = followPointVector - _flockingVector;
+                forceDirection.Normalize();
+                islandRigidBody.AddForce(forceDirection.normalized * followStrength, ForceMode.Impulse);
             }
         }
         
@@ -91,18 +91,109 @@ namespace Island.CollectableIsland
         /// Causes the island to rotate towards its follow point
         /// </summary>
         /// <returns>Lean tween descriptor so the update function can tell when the tweening is complete</returns>
-        private LTDescr RotateTowardsIsland()
+        private void RotateTowardsIsland()
         {
             //Calculate euler angle rotation required to rotate to face follow point
             var islandRotationVector = Vector3.RotateTowards(Vector3.one, followPoint.position, 1f, 10f);
-            var islandRotationEuler = Quaternion.FromToRotation(transform.forward, islandRotationVector).eulerAngles;
+            var transformRotation = Quaternion.LookRotation(islandRotationVector);
+            transformRotation.x = 0f;
+            transformRotation.z = 0f;
+            transform.rotation = transformRotation;
+        }
+
+        public void Flock(List<GameObject> trailingIslands)
+        {
+            Vector3 separation = CalculateSeparation(trailingIslands) * 1.5f;
+
+            Vector3 alignment = CalculateAlignment(trailingIslands);
+
+            Vector3 cohesion = CalculateCohesion(trailingIslands);
+
+            _flockingVector = separation + alignment + cohesion;
+            _flockingVector.Normalize();
+            _flockingVector *= flockingStrength;
+        }
+
+        private Vector3 CalculateCohesion(List<GameObject> trailingIslands)
+        {
+            //Calculate cohesion
+            //Calculate average position
+            float neighbourDist = 70;
+            int count = 0;
+            Vector3 sum = Vector3.zero;
+            //Sum positions from neighbouring islands (within neighbour distance)
+            //Ignore own position
+            foreach (var trailingIsland in from trailingIsland in trailingIslands 
+                where trailingIsland.GetInstanceID() != gameObject.GetInstanceID() 
+                let distance = Vector3.Distance(transform.position, trailingIsland.transform.position) 
+                where distance > 0 && distance < neighbourDist 
+                select trailingIsland)
+            {
+                sum += trailingIsland.transform.position;
+                count++;
+            }
+
+            if (count > 0)
+            {
+                //Divide by count to get average
+                sum /= trailingIslands.Count;
+            }
             
-            //Ignore rotation on x and z axis
-            islandRotationEuler.x = 0f;
-            islandRotationEuler.z = 0f;
-            
-            //Start rotation tweening utilizing lean tween (rotates over the rotation time)
-            return LeanTween.rotate(this.gameObject, islandRotationEuler, rotationTime);
+            //Return vector to average position
+            return transform.position - sum;
+        }
+
+        private Vector3 CalculateAlignment(List<GameObject> trailingIslands)
+        {
+            //Calculate alignment
+            //Calculate average velocity
+            float neighbourDist = 70;
+            int count = 0;
+            Vector3 sum = Vector3.zero;
+            //Sum velocities from neighbouring islands (within neighbour distance)
+            //Ignore own velocity
+            foreach (var trailingIsland in from trailingIsland in trailingIslands 
+                where trailingIsland.GetInstanceID() != gameObject.GetInstanceID() 
+                let distance = Vector3.Distance(transform.position, trailingIsland.transform.position) 
+                where distance > 0 && distance < neighbourDist 
+                select trailingIsland)
+            {
+                sum += trailingIsland.GetComponent<Rigidbody>().velocity;
+                count++;
+            }
+
+            if (count > 0)
+            {
+                //Divide by count to get average
+                sum /= trailingIslands.Count;
+            }
+            return sum;
+        }
+
+        private Vector3 CalculateSeparation(List<GameObject> trailingIslands)
+        {
+            //Calculate separation force
+            float desiredSep = gameObject.transform.localScale.x * 50f / 3f;
+            Vector3 sum = Vector3.zero;
+            int count = 0;
+            foreach (var trailingIsland in trailingIslands)
+            {
+                if (trailingIsland.GetInstanceID() == gameObject.GetInstanceID()) continue;
+                float distance = Vector3.Distance(transform.position, trailingIsland.transform.position);
+                if (!(distance > 0) || !(distance < desiredSep)) continue;
+                Vector3 difference = transform.position - trailingIsland.transform.position;
+                difference.Normalize();
+                difference /= distance;
+                sum += difference;
+                count++;
+            }
+
+            if (count > 0)
+            {
+                sum /= count;
+            }
+
+            return sum;
         }
     }
 }
