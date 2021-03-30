da6acac5df831d6289295fda04a378fd885be37f
Wayd <waydbarton@hotmail.co.nz>
2021-03-30 14:12:37 +1300

    Push for Jack

---
+++ Assets/Lava.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    #region Setup
+    GrappleChallengeMaster gcm;
+    private void Awake()
+    {
+        gcm = GetComponentInParent<GrappleChallengeMaster>();
+    }
+    #endregion Setup
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>())
+        {
+            // ResetMe
+            gcm.ResetChallenge();
+        }
+    }
+}
