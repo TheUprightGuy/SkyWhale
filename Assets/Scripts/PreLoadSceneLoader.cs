7364d1a82a12c6fbc4982ca02d119bdd61a99370
Jacob Gallagher <Jacob.Gallagher1@mds.ac.nz>
2021-04-19 13:02:18 +1200

    Preloadscene skip menu option

--- Assets/Scripts/PreLoadSceneLoader.cs
+++ Assets/Scripts/PreLoadSceneLoader.cs
@@ -5,9 +5,11 @@ using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class PreLoadSceneLoader : MonoBehaviour
 {
+    public bool startNewGameImmediately;
     //Uses start so that errors appear before scene change and log clearing
     #region Singleton
     public static PreLoadSceneLoader instance;
@@ -30,6 +32,19 @@ public class PreLoadSceneLoader : MonoBehaviour
         Debug.Log("Test preload scene logs");
         ClearLog();
         SceneManager.LoadScene("PreloadScene");
+    }
+    
+    private void Awake()
+    {
+        SceneManager.activeSceneChanged += SceneManagerOnactiveSceneChanged;
+    }
+
+    private void SceneManagerOnactiveSceneChanged(Scene arg0, Scene arg1)
+    {
+        //Return if not in menu scene or if immediate start is set to false
+        if(SceneManager.GetActiveScene().name != "MainMenu" || !startNewGameImmediately) return;
+        //Trigger immediate game start
+        GameObject.FindGameObjectWithTag("MainMenuCanvas").BroadcastMessage("ToggleMenuOption", MenuOptions.NewGame);
     }
     
     public void ClearLog()
