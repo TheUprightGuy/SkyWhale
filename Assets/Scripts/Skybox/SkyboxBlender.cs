43e51adcf1e237accb21abc00f08eca7cdb9a429
Wayd <waydbarton@hotmail.co.nz>
2021-05-12 18:08:00 +1200

    Skyblending

--- Assets/Scripts/Skybox/SkyboxBlender.cs
+++ Assets/Scripts/Skybox/SkyboxBlender.cs
@@ -4,32 +4,42 @@ using System.Collections;
 public class SkyboxBlender : MonoBehaviour
 {
     public Material material;
-    float blend;
-    float count;
+    float timeOfDay = 0.0f;
     public float blendSpeed = 1.0f;
 
-    public Color color1;
+   /* public Color color1;
     public Color color2;
     public Color color;
 
-    Light worldLight;
+    Light worldLight;*/
 
     void Start()
     {
-        worldLight = LightRotator.instance.worldLight;
-        material.SetFloat("_Blend", 0f);
-        color1 = material.GetColor("_Tint1");
-        color2 = material.GetColor("_Tint2");
+       
+        //worldLight = LightRotator.instance.worldLight;
+        //material.SetFloat("_Blend", 0f);
+        //color1 = material.GetColor("_Tint1");
+        //color2 = material.GetColor("_Tint2");
     }
+
 
     void Update()
     {
-        count += Time.deltaTime * blendSpeed;
+        timeOfDay += Time.deltaTime * blendSpeed;
+        if (timeOfDay >= 1.5f)
+        {
+            timeOfDay = 0.0f;
+        }
 
-        blend = Mathf.PingPong(count, 1.0f);
-        material.SetFloat("_Blend", blend);
-        color = Color.Lerp(color1, color2, blend);
-        worldLight.color = color;
+        material.SetFloat("_Vec1TimeOfDay", timeOfDay);
+
+
+       // count += Time.deltaTime * blendSpeed;
+
+        //blend = Mathf.PingPong(count, 1.0f);
+       // material.SetFloat("_Blend", blend);
+        //color = Color.Lerp(color1, color2, blend);
+       // worldLight.color = color;
     }
 }
 
