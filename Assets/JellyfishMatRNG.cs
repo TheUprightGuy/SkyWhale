d76a2a98a5e7efc1cddeda64b3ede1db7b5bb06b
Wayd <waydbarton@hotmail.co.nz>
2021-05-17 17:46:18 +1200

    JellyfishShader

---
+++ Assets/JellyfishMatRNG.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyfishMatRNG : MonoBehaviour
+{
+    Material mat1;
+    Material mat2;
+
+    private void Awake()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        mat1 = mr.materials[0];
+        mat2 = mr.materials[1];
+
+        Vector3 top = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        Vector3 bot = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+
+        mat1.SetVector("_Vector3", top);
+        mat2.SetVector("_Vector3", bot);
+    }
+}
