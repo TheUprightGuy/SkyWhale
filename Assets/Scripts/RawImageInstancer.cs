a88b291354d3f8e8f6715ddb92eb2ae7e9a77eba
Jack <jack.belton@hotmail.com>
2021-05-17 17:16:00 +1200

    MiniMap Implemented

---
+++ Assets/Scripts/RawImageInstancer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[ExecuteInEditMode]
+public class RawImageInstancer : MonoBehaviour
+{
+    private void Awake()
+    {
+        Material mat = Instantiate(GetComponent<RawImage>().material);
+        GetComponent<RawImage>().material = mat;
+    }
+}
