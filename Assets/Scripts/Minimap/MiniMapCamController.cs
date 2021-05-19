7cf0d4198ad7c09c4bb2bab3f6742761920654f1
Jack <jack.belton@hotmail.com>
2021-05-19 15:52:42 +1200

    Auto stash before merge of "JacksBranch" and "origin/JacksBranch"

--- Assets/Scripts/Minimap/MiniMapCamController.cs
+++ Assets/Scripts/Minimap/MiniMapCamController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class MiniMapCamController : MonoBehaviour
 {
+    [Header("Dependencies")]
     public LayerMask IslandLayers;
 
-    private GameObject CurrentTrackingObj;
+    private GameObject CurrentTrackingObj => (TrackingObj.activeSelf) ? (TrackingObj) : (WhaleTrackingObj); //Set tracking object to whale if playercontainer is turned off
     public GameObject TrackingObj;
     public GameObject WhaleTrackingObj;
 
     private GameObject CurrentObj = null;
     private List<SphereCollider> IslandObjColliders = new List<SphereCollider>();
 
+    private Camera thisCam;
 
+    public GameObject MinimapIcon;
+    public Vector3 IconOffset = Vector3.zero;
+    [Header("MiniMap")]
+    public float MiniMapZoom = 100.0f;
     private void Start()
     {
         GameObject[] gos = FindObjectsOfType(typeof(GameObject)) as GameObject[];
@@ -24,26 +30,37 @@ public class MiniMapCamController : MonoBehaviour
                 IslandObjColliders.Add(go.GetComponent<SphereCollider>());
             }
         }
+        thisCam = GetComponent<Camera>();
+        MinimapIcon.SetActive(true); 
     }
 
   
-
+    
     // Update is called once per frame
     void Update()
     {
-        CurrentTrackingObj = (TrackingObj != null) ? (TrackingObj) : (WhaleTrackingObj); //Set tracking object to whale if playercontainer is null
+        CurrentObj = CheckOnIsland(); //Update the current stood on island
+
+        transform.forward = CurrentTrackingObj.transform.forward;
+        transform.position = CurrentObj.transform.position - (transform.forward * MiniMapZoom);
+        thisCam.orthographicSize = MiniMapZoom;
+
+        if (MinimapIcon != null)
+        {
+            MinimapIcon.transform.position = CurrentTrackingObj.transform.position + IconOffset;
+        }
     }
 
     GameObject CheckOnIsland()
     {
-        GameObject returnObj = null;
+        GameObject returnObj = CurrentTrackingObj; //If no island is stood on, we want to track the map to the tracking obj
 
         foreach (var item in IslandObjColliders)
         {
-
-            if (true)
+            float distToObj = Vector3.Distance(item.gameObject.transform.position, CurrentTrackingObj.transform.position);
+            if (distToObj < item.radius) //Check if the current tracking obj is within the radius of the sphere collider
             {
-
+                return (item.gameObject);
             }
         }
         return (returnObj);
