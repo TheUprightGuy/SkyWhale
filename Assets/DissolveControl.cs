6d08ae8f39973a1b83d473940c7929fb3e3e62a9
Wayd <waydbarton@hotmail.co.nz>
2021-05-31 14:23:19 +1200

    Dissolve Shader for Jacob

---
+++ Assets/DissolveControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveControl : MonoBehaviour
+{
+    Material dissolveMat;
+    float startY, offsetY;
+    public bool dissolve;
+    private void Awake()
+    {
+        startY = transform.position.y + 0.5f;
+        offsetY = startY;
+
+        dissolveMat = GetComponent<MeshRenderer>().material;
+        dissolveMat.SetFloat("CutoffHeight", offsetY);
+    }
+
+    private void Update()
+    {
+        if (!dissolve)
+        {
+            offsetY = Mathf.Lerp(offsetY, startY + 1.0f, Time.deltaTime);
+            dissolveMat.SetFloat("CutoffHeight", offsetY);
+            return;
+        }
+
+        offsetY = Mathf.Lerp(offsetY, startY - 2.0f, Time.deltaTime);
+        dissolveMat.SetFloat("CutoffHeight", offsetY);
+    }
+
+}
