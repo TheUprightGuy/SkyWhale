38477651933a6cf99af41d70ee0900de5c7aad0c
Wayd <waydbarton@hotmail.co.nz>
2021-05-31 13:10:56 +1200

    ScriptFix Attempt

---
+++ Assets/WhaleCamp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleCamp : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToggleObjects(false);
+        EventManager.StartListening("ReturnWife", ToggleCampOn);
+    }
+
+    public void ToggleCampOn()
+    {
+        ToggleObjects(true);
+        EventManager.StopListening("ReturnWife", ToggleCampOn);
+    }
+
+    public void ToggleObjects(bool _toggle)
+    {
+        foreach (GameObject n in objects)
+        {
+            n.SetActive(_toggle);
+        }
+    }
+}
