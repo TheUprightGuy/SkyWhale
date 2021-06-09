5012cd204174ea36226d653271cce3f65999e34d
Wayd <waydbarton@hotmail.co.nz>
2021-06-09 14:46:12 +1200

    Rings Dissolving on Touch

---
+++ Assets/RingDissolver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingDissolver : MonoBehaviour
+{
+    Material mat;
+    public GameObject ring;
+    private void Awake()
+    {
+        mat = ring.GetComponent<MeshRenderer>().sharedMaterial = Instantiate(ring.GetComponent<MeshRenderer>().sharedMaterial);
+        startPos = transform.position.x;
+        fadeAmount = startPos;
+    }
+
+    public bool fade;
+    public float fadeAmount;
+    float startPos;
+
+    private void Update()
+    {
+        fadeAmount = fade ? fadeAmount - Time.deltaTime * 30.0f : fadeAmount + Time.deltaTime * 30.0f;
+        fadeAmount = Mathf.Clamp(fadeAmount, startPos - 50.0f, startPos + 65.0f);
+
+        mat.SetFloat("CutoffHeight", fadeAmount);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WhaleMovement wm = other.GetComponent<WhaleMovement>();
+        if (wm)
+            fade = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        WhaleMovement wm = other.GetComponent<WhaleMovement>();
+        if (wm)
+            fade = false;
+    }
+}
